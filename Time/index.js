381f5818355261602f153072e60106af5514a5a1
Petteri Kaikkonen <petteri.kaikkonen@gmail.com>
2019-04-07 13:24:58 +0300

    Update index.js
    debugging string formatting

--- Time/index.js
+++ Time/index.js
@@ -9,7 +9,7 @@ module.exports = async function(context, req) {
         `outputting a string in a format of "${req.query.locale}"`
       );
       context.res = {
-        body: `${datetime.toLocaleString("fi-FI")}`
+        body: `${datetime.toLocaleDateString("fi")}`
       };
     } else {
       context.log.info("outputting a string");
