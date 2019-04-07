008a8b61a4b1d55d64d3b85bbb7fb0353ceb9dbe
Petteri Kaikkonen <petteri.kaikkonen@gmail.com>
2019-04-07 13:42:02 +0300

    Cleaning up

--- OutputString/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function(context, req) {
-  context.log("JavaScript HTTP trigger function processed a request.");
-
-  context.res = {
-    body: `${new Date()}` // format as string just for prettier output in browser
-  };
-};
